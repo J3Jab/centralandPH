8b496764dc5e5532f334213ab4269c0b3616d5a8
giologizt <gio_montecillo@dlsu.edu.ph>
2023-08-02 13:49:41 +0800

    vercel analytics on ads

--- src/pages/api/ads/generate_big_ad.ts
+++ src/pages/api/ads/generate_big_ad.ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import path from "path";
 import { promises as fs } from "fs";
-
+import va from '@vercel/analytics';
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
   ) {
+    va.track('Big_Ad');
     const smallAdsDirectory = path.join(process.cwd(), "/public/ads/big-ads");
   
     // Load static data from file system
